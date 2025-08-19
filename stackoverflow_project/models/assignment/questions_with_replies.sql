1f225262cdbb3d907dcee16dbe2fcbd77727a173
Shipra Gupta <shipra.seraphs@gmail.com>
2025-08-19 23:15:53 +1200

    models created

---
+++ stackoverflow_project/models/assignment/questions_with_replies.sql
@@ -0,0 +1,18 @@
+-- models/questions_with_replies.sql
+
+WITH user_replies AS (
+  SELECT 
+    c.postid,
+    c.userid AS reply_user_id,
+    p.OwnerUserId AS question_user_id,
+    p.body
+  FROM {{ source('stackoverflow', 'comments') }} c
+  JOIN {{ source('stackoverflow', 'posts') }} p
+    ON c.postid = p.id
+  WHERE p.posttypeid = 1  -- Only questions
+)
+SELECT 
+  COUNT(DISTINCT ur.postid) AS reply_count
+FROM user_replies ur
+WHERE ur.reply_user_id != ur.question_user_id  -- Only replies to others, not the original poster
+  AND ur.body LIKE '%<>%'  -- <p> indicates a reply
---
+++ stackoverflow_project/models/assignment/top_10_users_by_views.sql
@@ -0,0 +1,18 @@
+-- models/top_10_users_by_views.sql
+
+WITH user_views AS (
+    SELECT 
+        p.id,
+        SUM(p.viewcount) AS total_views
+    FROM {{ source('stackoverflow', 'posts') }} p
+    WHERE p.PostTypeId = 'question'
+    GROUP BY p.id
+)
+SELECT 
+    u.id, 
+    u.displayname, 
+    upv.total_views
+FROM user_views upv
+JOIN {{ source('stackoverflow', 'users') }} u ON upv.id = u.id
+ORDER BY upv.total_views DESC
+LIMIT 10
